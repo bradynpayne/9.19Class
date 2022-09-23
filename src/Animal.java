08ac55f8ecf907f1f86105e789d48b72aa64a8d0
bradynpayne <brady_payne25@milton.edu>
2022-09-23 14:14:30 -0400

    After 9/23 Class

---
+++ src/Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+    private String name;
+
+    Animal(String x) {
+        this.name = x;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String x) {
+        this.name = x;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -18,7 +22,61 @@ public class Main {
 //        System.out.println(student1.attendance);
 //        System.out.println(student2.name);
 
-        Human human1 = new Human("Brady", 65, 10);
-        human1.printAttributes();
+//        Human human1 = new Human("Brady", 65, 10);
+//        human1.printAttributes();
+
+//        HashMap<String, Integer> attendance = new HashMap<>();
+//        attendance.put("Mike", 1);
+//        attendance.put("Charlie", 3);
+//        attendance.put("Jerald",5);
+//        attendance.put("Jeff",6);
+//
+//        for(String i: attendance.keySet()) {
+//            System.out.println(i);
+//        }
+
+//        HashMap<String, Boolean> attendance = new HashMap();
+//        ArrayList<String> present = new ArrayList();
+//        attendance.put("Mike", false);
+//        attendance.put("Jeff", true);
+//        attendance.put("Gerald", true);
+//        attendance.put("Sam", false);
+//
+//        for(String i: attendance.keySet()) {
+//            if (attendance.get(i) == true) {
+//                present.add(i);
+//            }
+//        }
+//        System.out.println(present);
+
+//        try{
+//            Scanner scanner = new Scanner(System.in);
+//            System.out.println("Enter a whole number to divide");
+//            int x = scanner.nextInt();
+//            System.out.println("Enter a whole number to divide by");
+//            int y = scanner.nextInt();
+//            int z = x/y;
+//            System.out.println("The result is:");
+//            System.out.println(z);
+//
+//        } catch(ArithmeticException e) {
+//            Scanner scanner = new Scanner(System.in);
+//            System.out.println("Try again");
+//            System.out.println("Enter a whole number to divide");
+//            int x = scanner.nextInt();
+//            System.out.println("Enter a whole number to divide by");
+//            int y = scanner.nextInt();
+//            System.out.print("The result is:");
+//            int z = x/y;
+//            System.out.println(z);
+//
+//        }
+
+        Animal Rhino = new Animal("Joe");
+        System.out.println(Rhino.getName());
+        Rhino.setName("Bob");
+        System.out.println(Rhino.getName());
+
+
     }
 }
